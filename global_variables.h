1d6c8f312af14c0f367309af79187772993b7116
JMLTUnderCode <juniormiguel08062@gmail.com>
2023-02-17 15:21:33 -0400

    modificacion y arreglos

--- global_variables.h
+++ global_variables.h
@@ -5,23 +5,21 @@
 #include <sys/types.h>
 #include <ctype.h>
 
-// Macros para True and False.
-#define TRUE (0==0)
-#define FALSE !TRUE
+#define TRUE (0==0)  			// Macro de TRUE del lenguaje.
+#define FALSE !TRUE 			// Macro de FALSE del lenguaje.
+#define SLOTS_HASH 97 		// Longitud constante de la tabla de hash.
+#define SLOTS_TWEET 281 	// Numero maximo de caracteres para un tweet.
 
-// Hass length
-#define SLOTS_HASH 97
+struct user_data; 				// Nodo informativo de un usuario.
+struct info_tweets; 			// Nodo informativo de un tweet.
+struct list_users; 				// Lista enlazada que usa nodo de tipo USER_DATA.
+struct list_tweets; 			// Lista enlazada que usa nodo de tipo INFO_TWEETS.
 
-struct list_user;
-typedef struct list_user list_user;
-
-struct user_data;
+// Definiciones de tipo de datos.
+typedef struct tm tm_t;
 typedef struct user_data user_data;
-
-struct list_timeline;
-typedef struct list_timeline list_timeline;
-
-struct list_tweets;
+typedef struct info_tweets info_tweets;
+typedef struct list_users list_users;
 typedef struct list_tweets list_tweets;
 
 // Definicion, inicializacion y estructuras de datos.
@@ -30,29 +28,29 @@ char username[17]; 				// Para username del usuario, MAX 16 Chars.
 char password[17]; 				// Para password del usuario, MAX 16 chars/digitos.
 user_data *INFO_USER; 		// Informacion clobal del usuario que esta logeado.
 
-struct list_tweets{
-	char tweet;
-	struct list_tweets *next;
+struct list_timeline{
+	char user;
+	tm_t time;
+	char message[SLOTS_TWEET];
 };
 
 struct user_data{
 	char user;
 	int password_h;
 	list_tweets *tweets;
- 	list_user *following; // Los que sigo.
-	list_user *followers; // Los que me siguen.
-	list_timeline *timeline;
+ 	list_users *following; 
+	list_users *followers;
+	info_tweets *timeline;
 };
 
-struct list_user{
+struct list_users{
 	user_data nodo;
 	list_user *next;
 };
 
-struct list_timeline{
-	char user;
-	struct tm time;
-	char message;
+struct list_tweets{
+	info_tweets tweets;
+	struct list_tweets *next;
 };
 
 /**/
